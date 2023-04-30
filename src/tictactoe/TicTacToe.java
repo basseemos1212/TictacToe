4ff173429cb6e54104dee6f9b13b0c89a1a04f3c
bassemos1212 <bassemosama1212@gmail.com>
2023-04-30 21:54:03 +0300

    test

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -29,7 +29,7 @@ public class TicTacToe extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("About.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("SplashUI.fxml"));
         Scene scene = new Scene(root, 1024, 700);
 
 //    Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
