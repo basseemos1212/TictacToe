f6f49a457f3034f3bb8acd83699c8fda2d4d872d
Mohamed-Hamza <mohamedhamzathedev@gmail.com>
2023-04-30 14:43:21 +0300

    Edit TicTacToe.java

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -5,11 +5,18 @@
  */
 package tictactoe;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  *
@@ -17,21 +24,52 @@ import javafx.stage.Stage;
  */
 public class TicTacToe extends Application {
     
+   private Font myCustomFont;
+
+
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("SignIn.fxml"));
-        
-        Scene scene = new Scene(root);
-        
+        Parent root = FXMLLoader.load(getClass().getResource("SplashUI.fxml"));
+        Scene scene = new Scene(root, 800, 600);
+
+//    Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         stage.setScene(scene);
+
+        PauseTransition delay = new PauseTransition(Duration.seconds(3));
+
+        // Navigate to the first screen after 3 seconds
+        delay.setOnFinished(event -> {
+            try {
+                // Load the FXML file for the first screen
+                Parent firstScreenRoot = FXMLLoader.load(getClass().getResource("SignUp.fxml"));
+
+                Scene firstScreenScene = new Scene(firstScreenRoot, 800, 600);
+                stage.setScene(firstScreenScene);
+
+            } catch (IOException ex) {
+                Logger.getLogger(TicTacToe.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+
         stage.show();
+        delay.play();
+
     }
+
+    @Override
+    public void init() throws Exception {
+        super.init(); 
+        myCustomFont = Font.loadFont(getClass().getResourceAsStream("/fonts/gumbo.otf"), 15);
+
+    }
+    
+    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
+
     }
-    
 }
