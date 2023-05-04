e381a323c844e469bdf283100d27a51702cfcf55
Amon3m <ahmed.50011.am@gmail.com>
2023-05-04 09:55:12 +0300

    login exc

--- src/tictactoe/SignInController.java
+++ src/tictactoe/SignInController.java
@@ -29,6 +29,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -53,6 +54,8 @@ public class SignInController implements Initializable {
     private Button signInBtn;
     
     private Player player;
+    @FXML
+    private Label loginStatusLbl;
    //HomeScreenController homeScreenController=new HomeScreenController(player) ;
     /**
      * Initializes the controller class.
@@ -153,7 +156,22 @@ public class SignInController implements Initializable {
             if (player.getStatus() == 1) {
                 goToHome(event, player);
             }
-            //navigate(event, "HomeScreen.fxml");
+            else if (player.getStatus() == -1) {
+                System.out.println("player not found!");
+                  loginStatusLbl.setVisible(true);
+                 loginStatusLbl.setText("player not found!");
+            }
+            
+            else {
+                 System.out.println("Incorrect Password");      
+                 loginStatusLbl.setVisible(true);
+                 loginStatusLbl.setText("Incorrect Password");
+
+
+// <Label fx:id="loginStatusLbl" layoutX="464.0" layoutY="419.0" styleClass="normalText" text="Incorrect Password" textFill="#e80a0a" visible="false" />
+            }
+
+
 
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(SignInController.class.getName()).log(Level.SEVERE, null, ex);
